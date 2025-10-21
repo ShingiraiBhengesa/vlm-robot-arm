fd6c287c62e657842525f175c6990958606d0817
ShingiraiBhengesa <shingiebhengesa@gmail.com>
2025-10-20 19:38:10 -0500

    update

--- src/my_robot_bringup/setup.py
+++ src/my_robot_bringup/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files from the 'launch' directory
+        # This is the crucial part that finds and installs your launch files
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
